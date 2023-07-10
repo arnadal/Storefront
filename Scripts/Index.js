9ba232e8b61782b5f11edf1467df7c6d4e2bc9d0
AR Nadal Verma <arnadal2010@gmail.com>
2023-07-10 13:12:20 +0530

    Major Updates: UI, Content, and Features

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -1,5 +1,17 @@
 let Theme = `Dark`;
 
+if (localStorage.getItem(`Theme`) == null) {
+
+  localStorage.setItem(`Theme`, Theme);
+
+} else {
+
+  Theme = localStorage.getItem(`Theme`);
+  if (Theme == `Dark`) document.querySelector(`:root`).classList.replace(`LightTheme`, `DarkTheme`);
+  if (Theme == `Light`) document.querySelector(`:root`).classList.replace(`DarkTheme`, `LightTheme`);
+
+}
+
 if (localStorage.getItem(`Cart`) == null) {
 
   localStorage.setItem(`Cart`, JSON.stringify([]));
@@ -12,13 +24,39 @@ function ToggleTheme() {
 
     document.querySelector(`:root`).classList.replace(`LightTheme`, `DarkTheme`);
     Theme = `Dark`;
+    localStorage.setItem(`Theme`, Theme);
 
   } else {
 
     document.querySelector(`:root`).classList.replace(`DarkTheme`, `LightTheme`);
     Theme = `Light`;
+    localStorage.setItem(`Theme`, Theme);
 
   }
+
+};
+
+function Notify(Positivity, Message) {
+
+  let NewElement = document.createElement(`div`);
+  NewElement.innerHTML = Message;
+  NewElement.setAttribute(`style`, `
+
+  background: var(--Color4);
+  border-left: 7.5px ${(Positivity) ? `#59FF64` : `#FF9F82`} solid;
+  color: #EEEEEE;
+  border-radius: var(--HighBorderRadius);
+  position: fixed;
+  right: 1%;
+  bottom: 1%;
+
+  `);
+
+  document.body.appendChild(NewElement);
+
+  setTimeout(function() {
+    NewElement.remove();
+  }, 5000);
 
 };
 
@@ -42,3 +80,90 @@ function AddToCart(ItemName) {
   localStorage.setItem(`Cart`, JSON.stringify(Cart));
 
 };
+
+console.log(window.location.pathname);
+
+function ClearCart() {
+
+  localStorage.setItem(`Cart`, JSON.stringify([]));
+  Cart = JSON.parse(localStorage.getItem(`Cart`));
+  document.querySelector(`#CheckoutStartButton`).setAttribute(`disabled`, `true`);
+  RenderItems(Cart);
+  RenderCartData(EvaluateData(JSON.parse(localStorage.getItem(`Cart`))))
+
+};
+
+function StartCheckout() {
+
+  document.querySelector(`form#CheckoutForm`).style.display = `initial`;
+  document.querySelector(`button#CheckoutStartButton`).style.display = `none`;
+
+};
+
+function Purchase() {
+
+  ClearCart();
+
+};
+
+function EvaluateData(Data = []) {
+
+  let Evaluation = {
+    TotalPrice_INR: 0,
+    Items: Data.length,
+    ItemKinds: 0
+  };
+
+  if (Data.length != 0) {
+
+    let IndexedItemKinds = [];
+
+    for (let Index = 0; Index < Data.length; Index++) {
+
+      let Element = Data[Index];
+
+      Evaluation.TotalPrice_INR += Number(Element.Price.replace(`₹`, ``));
+
+      if (!IndexedItemKinds.includes(Element.ItemName)) {
+
+        IndexedItemKinds.push(Element.ItemName);
+        Evaluation.ItemKinds++;
+
+      }
+
+    }
+
+    Evaluation.TotalPrice_INR = `₹${Evaluation.TotalPrice_INR}`;
+
+  }
+
+  return Evaluation;
+
+};
+
+function RenderCartData(EvaluationReport) {
+
+  document.querySelector(`span#CartData_TotalPrice`).innerHTML = EvaluationReport.TotalPrice_INR;
+  document.querySelector(`span#CartData_Items`).innerHTML = EvaluationReport.Items;
+  document.querySelector(`span#CartData_ItemKinds`).innerHTML = EvaluationReport.ItemKinds;
+
+};
+
+if (window.location.pathname == `/Cart.html`) RenderCartData(EvaluateData(JSON.parse(localStorage.getItem(`Cart`))));
+
+var prevScrollPos = window.pageYOffset || document.documentElement.scrollTop;
+var NavigationBar = document.querySelector(`#NavigationBar`);
+
+window.addEventListener("scroll", function() {
+  var currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (prevScrollPos > currentScrollPos) {
+    NavigationBar.classList.remove("hidden");
+  } else {
+    NavigationBar.classList.add("hidden");
+  }
+
+  prevScrollPos = currentScrollPos;
+});
+
+Notify(true);
--- Scripts/ItemFetcher.js
+++ Scripts/ItemFetcher.js
@@ -1,50 +1,65 @@
-function FetchItems() {
+let StoreItems;
+let Cart = [];
 
-  function LoadItems(Data) {
+async function FetchItems() {
+  async function LoadItems(data) {
+    StoreItems = data;
+  }
 
-    Data = JSON.parse(Data);
-    StoreItems = Data;
+  async function TransmitXHR(Filename) {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(xhr.responseText);
+          } else {
+            reject(new Error('Error: ' + xhr.status));
+          }
+        }
+      };
 
-  };
+      xhr.open('GET', Filename, true);
+      xhr.send();
+    });
+  }
 
-  function TransmitXHR(Filename) {
+  try {
+    const response = await TransmitXHR('Items.json');
+    const data = JSON.parse(response);
+    LoadItems(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-    let XHR = new XMLHttpRequest();
-    XHR.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        LoadItems(this.response);
-      }
-    };
-    XHR.open(`GET`, `${Filename}`, false);
-    XHR.send();
+function RenderItems(Data = []) {
 
-  };
+  if (Data.length == 0) {
 
-  TransmitXHR(`Items.json`);
+    if (window.location.pathname == `/Purchase.html`) {
 
-};
+      document.querySelector(`div#StoreItems`).innerHTML = `<p>Apologies! We could not find any item that you described. Try describing in short, and avoid being too specific.</p>`;
 
-function RenderItems(Data) {
+    } else if (window.location.pathname == `/Cart.html`) {
 
-  let UltimateParent = document.querySelector(`div#StoreItems`)
+      document.querySelector(`div#StoreItems`).innerHTML = `<p>Your cart is empty. You might want to <a href="Purchase.html">purchase some products</a>!</p>`;
+
+    }
+
+    return;
+
+  }
+
+  let UltimateParent = document.querySelector(`div#StoreItems`);
   UltimateParent.innerHTML = ``;
-  let PreviousParent = null;
 
   for (let Index = 0; Index < Data.length; Index++) {
 
-    let Parent = PreviousParent;
-
-    if (Index % 4 == 0) {
-      Parent = document.createElement(`div`);
-      Parent.className = `ItemCardRow`;
-      PreviousParent = Parent;
-    }
-
     let NewElement = document.createElement(`div`);
     NewElement.className = `ItemCard`;
-    NewElement.innerHTML = `<div class="ItemImage" style="background-image: url('${Data[Index].Image}');" alt="${Data[Index].ItemName}"></div><b class="ItemName">${Data[Index].ItemName}</b><br><span class="ItemPrice">${Data[Index].Price}</span><br>${(window.location.pathname == `/Purchase.html`) ? `<button id="AddToCartButton" onclick="AddToCart('${Data[Index].ItemName}')" style="position: relative; bottom: 0; right: 0; float: right;"><i class="material-icons">add_shopping_cart</i></button>` : ``}`;
-    Parent.appendChild(NewElement);
-    UltimateParent.appendChild(Parent);
+    NewElement.innerHTML = `<div class="ItemImage" style="background-image: url('${Data[Index].Image}');" alt="${Data[Index].ItemName}"></div><b class="ItemName">${Data[Index].ItemName}</b><span class="ItemPrice">${Data[Index].Price}</span><br>${(window.location.pathname.includes(`/Purchase.html`)) ? `<button id="AddToCartButton" onclick="AddToCart('${Data[Index].ItemName}')" style="position: relative; bottom: 0; right: 0;"><i class="material-icons">add_shopping_cart</i></button>` : ``}`;
+    UltimateParent.appendChild(NewElement);
 
   }
 
@@ -56,42 +71,60 @@ function SearchItem(Item) {
 
 };
 
-if (window.location.pathname == `/Purchase.html`) {
+async function MainCode() {
 
-  FetchItems();
-  RenderItems(StoreItems);
+  if (window.location.pathname.includes(`/Purchase.html`)) {
 
-  if (window.location.search.includes(`?Search=`)) {
+    await FetchItems();
+    RenderItems(StoreItems);
+    if (window.location.search.includes(`?Search=`)) {
 
-    function FetchSearch() {
+      function FetchSearch() {
 
-      let Search = decodeURI(window.location.search.replace(`?Search=`, ``));
-      let Matches = [];
+        let Search = decodeURI(window.location.search.replace(`?Search=`, ``));
+        let Matches = [];
 
-      for (let Index = 0; Index < StoreItems.length; Index++) {
+        for (let Index = 0; Index < StoreItems.length; Index++) {
 
-        console.log(Index, Search, Matches, StoreItems[Index]);
-        if (StoreItems[Index].ItemName.match(new RegExp(`${Search}`, `gi`)) != null) {
+          if (StoreItems[Index].ItemName.match(new RegExp(`${Search}`, `gi`)) != null) {
 
-          Matches.push(StoreItems[Index]);
-          console.log(Matches);
+            Matches.push(StoreItems[Index]);
+
+          }
 
         }
 
+        return Matches;
+
+      };
+
+      RenderItems(FetchSearch());
+
+    }
+
+  };
+
+  if (window.location.pathname.includes(`/Cart.html`)) {
+
+    Cart = JSON.parse(localStorage.getItem(`Cart`));
+    if (Cart.length == 0) document.querySelector(`#CheckoutStartButton`).setAttribute(`disabled`, `true`);
+    RenderItems(Cart);
+
+    setInterval(function() {
+
+      if (JSON.stringify(Cart) != localStorage.getItem(`Cart`)) {
+
+        Cart = JSON.parse(localStorage.getItem(`Cart`));
+        if (Cart.length == 0) document.querySelector(`#CheckoutStartButton`).setAttribute(`disabled`, `true`);
+        RenderItems(Cart);
+        RenderCartData(EvaluateData(JSON.parse(localStorage.getItem(`Cart`))));
+
       }
 
-      return Matches;
-
-    };
-
-    RenderItems(FetchSearch());
+    }, 250);
 
   }
 
-};
-
-if (window.location.pathname == `/Cart.html`) {
-
-  RenderItems(JSON.parse(localStorage.getItem(`Cart`)));
-
 }
+
+MainCode();
